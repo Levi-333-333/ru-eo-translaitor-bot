cc49444a3966781d53c866fb9fdc2b08f3f9d01a
Levi_333 <leva.nechaev2008@gmail.com>
2026-07-22 02:22:57 +0300

    project structure changed: handlers moved to a separate file.

---
+++ handlers.py
@@ -0,0 +1,14 @@
+from aiogram import Dispatcher, F
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from translate import translate
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, это бот-переводчик из русского языка на эсперанто! Принцип работы прост: вы пишете сообщение боту на русском - он отвечает на эсперанто, и наоборот!\nSaluton, mi estas roboto por tradukado inter la rusa kaj Esperanto! Ĝi funkcias simple: vi sendas al la roboto mesaĝon en la rusa, kaj ĝi respondas en Esperanto — kaj inverse!")
+
+@dp.message(F.text)
+async def text_message_handler(message: Message) -> None:
+    await message.reply(translate(message.text))
--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 from dotenv import load_dotenv
 from os import getenv
-
 import asyncio
 import logging
 import sys
-
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot
 from aiogram.client.session.aiohttp import AiohttpSession
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
-from translate import translate
+from handlers import *
 
 load_dotenv()
-
 TOKEN = getenv("BOT_TOKEN")
-dp = Dispatcher()
 
 PROXY_URL = "socks5://127.0.0.1:9050"
 session = AiohttpSession(proxy=PROXY_URL)
 
 bot = Bot(token=TOKEN, session=session)
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Привет, это бот-переводчик из русского языка на эсперанто! Принцип работы прост: вы пишете сообщение боту на русском - он отвечает на эсперанто, и наоборот!\nSaluton, mi estas roboto por tradukado inter la rusa kaj Esperanto! Ĝi funkcias simple: vi sendas al la roboto mesaĝon en la rusa, kaj ĝi respondas en Esperanto — kaj inverse!")
-
-@dp.message(F.text)
-async def text_message_handler(message: Message) -> None:
-    await message.reply(translate(message.text))
 
 async def main() -> None:
     await dp.start_polling(bot)
