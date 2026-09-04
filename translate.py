80040d476c08a8c26f56dccb022875c7ec08867e
Levi-333-333 <leva.nechaev2008@gmail.com>
2026-09-04 13:06:42 +0300

    сhange: The translation function is divided into a language detection function and the translation itself.

--- translate.py
+++ translate.py
@@ -1,19 +1,17 @@
 from dotenv import load_dotenv
 from deep_translator import GoogleTranslator
 
-def translate(text: str) -> str:
+def detect_leanguage(text:str) -> dict[str, str]:
     strip_text = text.strip()
     if not strip_text:
-        return ""
-
-    PROXY_URL = load_dotenv("PROXY_URL")
-    proxy = {
-        "http": PROXY_URL,
-        "https": PROXY_URL
-    }
+        raise Exception
 
     eo_special_chars = ['ĉ', 'ĝ', 'ĥ', 'ĵ', 'ŝ', 'ŭ', 'Ĉ', 'Ĝ', 'Ĥ', 'Ĵ', 'Ŝ', 'Ŭ']
-    eo_words = ('esperanto', 'birdo', 'domo', 'amiko', 'patro', 'libro', 'tago', 'jaro', 'bela', 'bona', 'nova', 'rapida', 'facile', 'bone', 'ami', 'vidi', 'esti', 'havi', 'iri', 'voli', 'povi', 'la', 'kaj', 'kun', 'por', 'pri', 'ĉar', 'tamen', 'sub', 'sur', 'apud', 'hodiaŭ', 'morgaŭ', 'tiam', 'ĉiam', 'kiam', 'tio', 'kio', 'ĉio', 'nenio', 'tiu', 'kiu', 'ĉiu', 'neniu', 'tie', 'kie', 'ĉie', 'nenie', 'tiel', 'kiel', 'ĉiel', 'neniel', 'tial', 'kial', 'ĉial', 'nenial')
+    eo_words = ('esperanto', 'birdo', 'domo', 'amiko', 'patro', 'libro', 'tago', 'jaro', 'bela', 'bona', 'nova',
+                'rapida', 'facile', 'bone', 'ami', 'vidi', 'esti', 'havi', 'iri', 'voli', 'povi', 'la', 'kaj', 'kun',
+                'por', 'pri', 'ĉar', 'tamen', 'sub', 'sur', 'apud', 'hodiaŭ', 'morgaŭ', 'tiam', 'ĉiam', 'kiam', 'tio',
+                'kio', 'ĉio', 'nenio', 'tiu', 'kiu', 'ĉiu', 'neniu', 'tie', 'kie', 'ĉie', 'nenie', 'tiel', 'kiel',
+                'ĉiel', 'neniel', 'tial', 'kial', 'ĉial', 'nenial')
     is_eo = any(word in strip_text for word in eo_words) or any(car in strip_text for car in eo_special_chars) or strip_text.isascii()
 
     if is_eo:
@@ -23,4 +21,17 @@ def translate(text: str) -> str:
         input_lang = 'ru'
         output_lang = 'eo'
 
-    return GoogleTranslator(source=input_lang, target=output_lang, proxies=proxy).translate(strip_text)
+    return {
+        "input_lang": input_lang,
+        "output_lang": output_lang
+    }
+
+def translate(text:str) -> str:
+    PROXY_URL = load_dotenv("PROXY_URL")
+    proxy = {
+        "http": PROXY_URL,
+        "https": PROXY_URL
+    }
+
+    langs = detect_leanguage(text)
+    return GoogleTranslator(source=langs["input_lang"], target=langs["output_lang"], proxies=proxy).translate(text)
